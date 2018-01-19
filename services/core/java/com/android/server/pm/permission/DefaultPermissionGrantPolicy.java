b9faad9020ee5f3c39c5761eca8ba38d384d73e3
Alex Cruz <du.alexcruz@gmail.com>
2018-01-18 23:14:52 -0500

    DefaultPermissionGrantPolicy: Fix Google Dialer FC due to missing permissions
    In past verisons you could not download the Google dialer unless
    you were on a Nexus/Pixel. Now that we're able to download it on pretty
    much every device running 8.1, we need to protect the user from the user.
    
    Some users are smarter than others, is that simple. Some users will realize
    that the reason this FCs is because of missing permissions. Some won't be that
    smart and will come to us and report this as a bug. This commit fixes this.
    
    [@AgentFabulous - POSP]
    - Adapt for android-10.0.0_r2
    - Use grantSystemFixedPermissionsToSystemPackage to simplify syntax
    
    Change-Id: I125b42f5c68bd84aece41f2f9124716af06a88d6
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
+++ services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
@@ -881,6 +881,10 @@ public final class DefaultPermissionGrantPolicy {
         // Chromium Sign-in
         grantSystemFixedPermissionsToSystemPackage(pm,"org.chromium.chrome", userId,
                 CONTACTS_PERMISSIONS, STORAGE_PERMISSIONS);
+
+        // Google Dialer
+        grantSystemFixedPermissionsToSystemPackage(pm,"com.google.android.dialer", userId,
+                PHONE_PERMISSIONS, CONTACTS_PERMISSIONS, SMS_PERMISSIONS);
     }
 
     private String getDefaultSystemHandlerActivityPackageForCategory(PackageManagerWrapper pm,
